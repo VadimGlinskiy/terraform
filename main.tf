f1ff9219b7d2ddca9951c10284bc4473bb786d4b
VadimGlinskiy <vadik.glinskij412@gmail.com>
2024-04-24 20:01:04 +0300

    Отправил файл состояния в s3 хранилище

--- main.tf
+++ main.tf
@@ -5,6 +5,22 @@ terraform {
     }
   }
   required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket = "vadim-terraform-backet"
+    region = "ru-central1"
+    key    = "terraform1.tfstate"
+    shared_credentials_file = "storage.key"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
 }
 
 
@@ -36,7 +52,7 @@ resource "yandex_vpc_security_group" "group1" {
   }
 
   dynamic "ingress" {
-    for_each = ["80", "443"]
+    for_each = ["80", "443", "8080"]
     content {
       protocol       = "TCP"
       description    = "rule1 description"
