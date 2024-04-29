fa5cceaef11bb8cec27946cd4e8c4973a2466f3b
VadimGlinskiy <vadik.glinskij412@gmail.com>
2024-04-29 14:52:59 +0300

    Инициализация и план кубер-кластера в Yandex.Cloud

---
+++ Kubernetes Cluster/main.tf
@@ -0,0 +1,176 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://storage.yandexcloud.net"
+    }
+    bucket                  = "vadim-terraform-backet"
+    region                  = "ru-central1"
+    key                     = "terraform2.tfstate"
+    shared_credentials_file = "storage.key"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true # Необходимая опция Terraform для версии 1.6.1 и старше.
+    skip_s3_checksum            = true # Необходимая опция при описании бэкенда для Terraform версии 1.6.3 и старше.
+
+  }
+}
+
+
+provider "yandex" {
+  service_account_key_file = "/home/vadim/key.json"
+  cloud_id                 = "b1g316ieaomifjl52bp5"
+  folder_id                = "b1g7i1ahrh2lvqf17o4t"
+  zone                     = "ru-central1-a"
+}
+
+locals {
+  cloud_id    = "b1g316ieaomifjl52bp5"
+  folder_id   = "b1g7i1ahrh2lvqf17o4t"
+  k8s_version = "1.22"
+  sa_name     = "myaccount"
+}
+
+resource "yandex_vpc_network" "my-regional-net" {
+  name = "my-regional-net"
+}
+
+resource "yandex_vpc_subnet" "mysubnet-a" {
+  name           = "mysubnet-a"
+  v4_cidr_blocks = ["10.5.0.0/16"]
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.my-regional-net.id
+}
+
+resource "yandex_vpc_subnet" "mysubnet-b" {
+  name           = "mysubnet-b"
+  v4_cidr_blocks = ["10.6.0.0/16"]
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.my-regional-net.id
+}
+
+resource "yandex_vpc_subnet" "mysubnet-d" {
+  name           = "mysubnet-d"
+  v4_cidr_blocks = ["10.7.0.0/16"]
+  zone           = "ru-central1-d"
+  network_id     = yandex_vpc_network.my-regional-net.id
+}
+
+resource "yandex_iam_service_account" "my-regional-account" {
+  name        = "regional-k8s-account"
+  description = "K8S regional service account"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "editor" {
+  # Сервисному аккаунту назначается роль "editor".
+  folder_id = "editor"
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+
+
+resource "yandex_resourcemanager_folder_iam_member" "images-puller" {
+  # Сервисному аккаунту назначается роль "container-registry.images.puller".
+  folder_id = local.folder_id
+  role      = "container-registry.images.puller"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "encrypterDecrypter" {
+  # Сервисному аккаунту назначается роль "kms.keys.encrypterDecrypter".
+  folder_id = local.folder_id
+  role      = "kms.keys.encrypterDecrypter"
+  member    = "serviceAccount:${yandex_iam_service_account.my-regional-account.id}"
+}
+
+resource "yandex_kms_symmetric_key" "kms-key" {
+  # Ключ Yandex Key Management Service для шифрования важной информации, такой как пароли, OAuth-токены и SSH-ключи.
+  name              = "kms-key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h" # 1 год.
+}
+
+resource "yandex_vpc_security_group" "regional-k8s-sg" {
+  name        = "regional-k8s-sg"
+  description = "Правила группы обеспечивают базовую работоспособность кластера Managed Service for Kubernetes. Примените ее к кластеру и группам узлов."
+  network_id  = yandex_vpc_network.my-regional-net.id
+  ingress {
+    protocol          = "TCP"
+    description       = "Правило разрешает проверки доступности с диапазона адресов балансировщика нагрузки. Нужно для работы отказоустойчивого кластера Managed Service for Kubernetes и сервисов балансировщика."
+    predefined_target = "loadbalancer_healthchecks"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol          = "ANY"
+    description       = "Правило разрешает взаимодействие мастер-узел и узел-узел внутри группы безопасности."
+    predefined_target = "self_security_group"
+    from_port         = 0
+    to_port           = 65535
+  }
+  ingress {
+    protocol       = "ANY"
+    description    = "Правило разрешает взаимодействие под-под и сервис-сервис. Укажите подсети вашего кластера Managed Service for Kubernetes и сервисов."
+    v4_cidr_blocks = concat(yandex_vpc_subnet.mysubnet-a.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-b.v4_cidr_blocks, yandex_vpc_subnet.mysubnet-d.v4_cidr_blocks)
+    from_port      = 0
+    to_port        = 65535
+  }
+  ingress {
+    protocol       = "ICMP"
+    description    = "Правило разрешает отладочные ICMP-пакеты из внутренних подсетей."
+    v4_cidr_blocks = ["10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"]
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "Правило разрешает входящий трафик из интернета на диапазон портов NodePort. Добавьте или измените порты на нужные вам."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 30000
+    to_port        = 32767
+  }
+  egress {
+    protocol       = "ANY"
+    description    = "Правило разрешает весь исходящий трафик. Узлы могут связаться с Yandex Container Registry, Yandex Object Storage, Docker Hub и т. д."
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    from_port      = 0
+    to_port        = 65535
+  }
+}
+
+resource "yandex_kubernetes_cluster" "k8s-regional" {
+  name                    = "k8s-regional"
+  network_id              = yandex_vpc_network.my-regional-net.id
+  network_policy_provider = "CALICO"
+  master {
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-a.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-a.id
+    }
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-b.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-b.id
+    }
+    master_location {
+      zone      = yandex_vpc_subnet.mysubnet-d.zone
+      subnet_id = yandex_vpc_subnet.mysubnet-d.id
+    }
+    security_group_ids = [yandex_vpc_security_group.regional-k8s-sg.id]
+  }
+  service_account_id      = yandex_iam_service_account.my-regional-account.id
+  node_service_account_id = yandex_iam_service_account.my-regional-account.id
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.editor,
+    yandex_resourcemanager_folder_iam_member.images-puller,
+    yandex_resourcemanager_folder_iam_member.encrypterDecrypter
+  ]
+  kms_provider {
+    key_id = yandex_kms_symmetric_key.kms-key.id
+  }
+}
